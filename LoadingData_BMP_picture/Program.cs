16f68922ca1d8afe2e058d3fe87a2921cc5039b1
batlicka <vojta@vagunda.eu>
2020-02-27 13:28:15 +0100

    added Note

--- LoadingData_BMP_picture/Program.cs
+++ LoadingData_BMP_picture/Program.cs
@@ -14,7 +14,7 @@ namespace LoadingData_BMP_picture
         
         static void Main(string[] args)
         {
-            
+            //note Vojta
             String fileName = "d:\\dokumenty\\Vojta\\UTB\\visual_studio_repos\\picturesSources\\jednoduchy1.bmp";
             FileStream fs = new FileStream("d:\\dokumenty\\Vojta\\UTB\\visual_studio_repos\\picturesSources\\jednoduchy1.bmp", FileMode.Open,FileAccess.Read);
             BinaryReader br = new BinaryReader(fs);
